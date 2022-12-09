12a4a1258ea76ff1753c088a761c29c50130be08
SACHIN K V <sachin.kv.1011@gmail.com>
2022-12-09 13:20:43 +0530

    initial

---
+++ src/main/java/com/example/OnlineComplaint_backend/OnlineComplaintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnlineComplaint_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineComplaintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineComplaintBackendApplication.class, args);
+	}
+
+}
